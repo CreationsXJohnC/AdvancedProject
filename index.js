3d03b26d0d6e1837be1a8996b480c24c68176e84
Che <johnccreations21@gmail.com>
2025-07-13 09:28:04 -0400

    fifth update

--- index.js
+++ index.js
@@ -2,6 +2,19 @@
 // service_busjp8s
 // OYylWZHPRCLJHLpX7
 
+let isModalOpen = false;
+let contrastToggle = false;
+
+function toggleContrast() {
+    contrastToggle = !contrastToggle;
+    if (contrastToggle) {
+        document.body.classList += " dark-theme"
+    }
+    else {
+        document.body.classList.remove("dark-theme")
+    }
+}
+
 
 function contact(event) {
     event.preventDefault();
@@ -23,4 +36,13 @@ function contact(event) {
                 "The email service is temporarily unavailable. Please contact me directly on johnccreations21@gmail.com"
             );
         })
+}
+
+function toggleModal() {
+    if (isModalOpen) {
+        isModalOpen = false;
+        return document.body.classList.remove("modal--open");
+    }
+    isModalOpen = true;
+    document.body.classList += " modal--open";
 }
